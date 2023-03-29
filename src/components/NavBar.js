86604da8ea222a6ac4675dfc684ec624ea472467
Pierce <piercedeanda@gmail.com>
2023-03-29 09:23:48 -0700

    added nav, fixed spacing on nav, and added navlinks

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 function NavBar() {
     return (
-        <section className="nav">
-            <h1>Nav Bar</h1>
-        </section>
+        <nav className="nav">
+            <div className="nav-routes">
+                <NavLink to="/" >
+                    Home
+                </NavLink>
+                <NavLink to="/about">
+                    About    
+                </NavLink>
+                <NavLink to="/projects">
+                    Projects
+                </NavLink>
+            </div>
+            <div className="nav-routes">
+                <a>LinkedIn</a>
+                <a>Github</a>
+            </div>
+        </nav>
     )
 }
 
